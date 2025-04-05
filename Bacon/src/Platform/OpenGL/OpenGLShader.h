9253e551214b2f575521e74e7c9eee6803be374f
AIdauv <ljc1320551158@163.com>
2025-04-05 20:17:12 +0800

    Shader Asset Files

--- Bacon/src/Platform/OpenGL/OpenGLShader.h
+++ Bacon/src/Platform/OpenGL/OpenGLShader.h
@@ -3,10 +3,14 @@
 #include "Bacon/Renderer/Shader.h"
 #include <glm/glm.hpp>
 
+// TODO: REMOVE!
+typedef unsigned int GLenum;
+
 namespace Bacon {
 	class OpenGLShader : public Shader
 	{
 	public:
+		OpenGLShader(const std::string& filepath);
 		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
@@ -22,6 +26,10 @@ namespace Bacon {
 
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+	private:
+		std::string ReadFile(const std::string& filepath);
+		std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+		void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
 	private:
 		uint32_t m_RendererID;
 	};
